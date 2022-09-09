eadad1619f91159d7dcab729e59ead3bd44e24f7
emba00 <emba@udel.edu>
2022-09-09 15:41:45 -0400

    Task 3 again!

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import kaido from "./images/kaido.png";
 function App(): JSX.Element {
     return (
         <div className="App">
-            <h1 className="App-header">
+            <h1 className="App-header" style={{ backgroundColor: "lightblue" }}>
                 Cisc275 w/ Emmanuel Mbah and The Weakest Yonko
             </h1>
             <img src={kaido} alt="KAIDOOOOO" width="250" height="150" />

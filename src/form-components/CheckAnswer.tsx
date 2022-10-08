11e5707d4f94268ea958b0e44ab72cecc587bf19
emba00 <emba@udel.edu>
2022-10-08 14:54:39 -0400

    uh trying to fix something

--- src/form-components/CheckAnswer.tsx
+++ src/form-components/CheckAnswer.tsx
@@ -1,12 +1,21 @@
 import React, { useState } from "react";
+import { Form } from "react-bootstrap";
 
 export function CheckAnswer({
     expectedAnswer
 }: {
     expectedAnswer: string;
 }): JSX.Element {
+    const [userAnswer, setuserAnswer] = useState<string>();
+    function updateAnswer(event: React.ChangeEvent<HTMLInputElement>) {
+        setuserAnswer(event.target.value)
+    }
     return (
         <div>
+            <Form.Group>
+                <Form.Label>Enter Answer:</Form.Label>
+                <Form.Control value={userAnswer} onChange={updateAnswer} />
+            </Form.Group>
             <h3>Check Answer</h3>
         </div>
     );
